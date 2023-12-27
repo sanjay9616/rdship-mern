05aa45d92ec60fb0c8868ab9d9cda05fe5164b61
sanjaymoglix <sanjay.kumar@moglix.com>
2023-12-27 22:17:35 +0530

    implemented dashboard account ui

---
+++ components/AllNotifications.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllNotifications = () => {
+  return (
+    <div>AllNotifications</div>
+  )
+}
+
+export default AllNotifications
---
+++ components/MyCoupons.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyCoupons = () => {
+  return (
+    <div>MyCoupons</div>
+  )
+}
+
+export default MyCoupons
