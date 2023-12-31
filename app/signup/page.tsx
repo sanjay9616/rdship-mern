dd991213be66d1c8f13e4878f20319add239b0ac
aditya maurya <adityamaurya9717@gmail.com>
2023-12-31 10:32:38 +0530

    change

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,27 +1,54 @@
 "use client"
-import { FormControl, InputLabel, OutlinedInput, TextField } from '@mui/material'
+import { Button, FormControl, InputLabel, OutlinedInput, TextField } from '@mui/material'
+
 import React, { useState } from 'react'
 
 type signUpRequest = {
-    email: string,
-    mobile: string,
-    password: string
+    email: {
+        value:string,
+        error:string
+    },
+    mobile: {
+        value:string,
+        error:string
+    },
+    password: {
+        value:string,
+        error:string
+    }
 }
 function page() {
     const [signUpForm, setSignUpForm] = useState<signUpRequest>({
-        email: '',
-        mobile: '',
-        password: ''
+        email: {
+            value: '',
+            error:''
+        },
+        mobile: {
+            value: '',
+            error:''
+        },
+        password: {
+            value: '',
+            error:''
+        }
     });
 
-    const onInputHandler = (e: any) => {
-        setSignUpForm(preState => {
-            return { ...preState, [e.target.name]: e.target.value }
-        })
+    const onInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setSignUpForm((prevForm) => ({
+          ...prevForm,
+          [name]: {
+            ...prevForm[name as keyof signUpRequest ],
+            value: value,
+          },
+        }));
+    }
+    const onSubmit = ()=>{
+        console.log(signUpForm)
     }
     return (
-        <section className="flex flex-col items-center justify-center w-full bg-white">
-            <div className='mt-4'>
+        <section className="flex flex-col items-center justify-center bg-white  w-full ">
+            <div className='mt-4 pb-2 bg-white'>
                 {/* Heading with bold Tag */}
                 <div>
                     <b>Sign Up</b>
@@ -30,11 +57,19 @@ function page() {
                 <div className='flex flex-row mt-2'>
                     <FormControl variant="outlined">
                         <InputLabel htmlFor="signup-email" >Enter Email</InputLabel>
-                        <OutlinedInput id="signup-email" name='email' type='text' label="Enter Email"></OutlinedInput>
+                        <OutlinedInput id="signup-email" onChange={onInputHandler} value={signUpForm.email.value} name='email' type='text' label="Enter Email"></OutlinedInput>
                     </FormControl>
                     <FormControl>
                         <InputLabel htmlFor="signup-mobile">Enter Mobile</InputLabel>
-                        <OutlinedInput id="signup-mobile" name="mobile" type="text" label="Enter Mobile"></OutlinedInput>
+                        <OutlinedInput 
+                             id="signup-mobile" 
+                             onChange={onInputHandler}
+                             value={signUpForm.mobile.value}
+                             name="mobile"
+                             type="text"
+                              label="Enter Mobile">
+
+                        </OutlinedInput>
                     </FormControl>
                 </div>
                 {/*  Input for password and Recorrect-password */}
@@ -45,8 +80,14 @@ function page() {
                     </FormControl>
                     <FormControl className='mt-3'>
                         <InputLabel htmlFor="signup-password">Re Enter Password</InputLabel>
-                        <OutlinedInput id="signup-password" name="mobile" type="text" label="Re Enter Password"></OutlinedInput>
+                        <OutlinedInput id="signup-password" name="mobile" type="text" label="Re Enter Password"
+                        ></OutlinedInput>
+
                     </FormControl>
+                    <FormControl className='mt-2'>
+                        <Button onClick={onSubmit} style={{ backgroundColor: 'blue', color: 'white' }} variant='contained'>Sign Up</Button>
+                    </FormControl>
+
                 </div>
             </div>
 
