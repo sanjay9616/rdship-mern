fd4811392d88d5632ca157455c02c6189f155569
shashanksingh2000 <shashank.singh@moglix.com>
2023-12-31 16:52:37 +0530

    Changes done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
+import './globals.css'
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
 import * as React from "react";
 
 const inter = Inter({ subsets: ['latin'] })
